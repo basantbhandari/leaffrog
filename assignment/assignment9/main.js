da585983d18405d575f4142332033b5dd49d1d2a
basantbhandari <basantbhandari2074@gmail.com>
2022-06-13 22:11:56 +0545

    assignment 9 submit

--- assignment/assignment9/main.js
+++ assignment/assignment9/main.js
@@ -22,23 +22,46 @@ function updateSlide(curSlide) {
 }
 
 updateSlide(curSlide)
+updateSlideEffect(curSlide)
+
 
 // add event listener and next slide functionality
 nextSlide.addEventListener("click", function () {
     // if current slide is not the last slide
-    if (curSlide < totalNumbeOfFrame - 1) {
-        curSlide++;
-
+    if (curSlide < totalNumbeOfFrame) {
         slides.forEach((slide, indx) => {
             slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
             updateSlide(curSlide)
+            updateSlideEffect(curSlide);
             prevSlide.style.display = "block";
         });
+        curSlide++;
     }else{
         nextSlide.style.display = "none";
         prevSlide.style.display = "block";
     }
 });
+
+setInterval(function(){
+    if (curSlide < totalNumbeOfFrame) {
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+            updateSlide(curSlide)
+            updateSlideEffect(curSlide);
+            prevSlide.style.display = "block";
+        });
+        curSlide++;
+    }else if(curSlide == (totalNumbeOfFrame-1)){
+        curSlide = 0;
+
+    }
+    else{
+        nextSlide.style.display = "none";
+        prevSlide.style.display = "block";
+    }
+   
+
+},1000*20)
 
 
 // add event listener and previous slide functionality
@@ -46,16 +69,92 @@ nextSlide.addEventListener("click", function () {
 prevSlide.addEventListener("click", function () {
     // if current slide is not the first slide
     if (curSlide > 0) {
-        curSlide--;
         slides.forEach((slide, indx) => {
             slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
             updateSlide(curSlide)
+            updateSlideEffect(curSlide);
             nextSlide.style.display = "block";
             }
         );
-    }else{
+        curSlide--;
+    }else if(curSlide == -1){
+        curSlide = totalNumbeOfFrame - 1
+
+    }
+    
+    else{
         prevSlide.style.display = "none";
         nextSlide.style.display = "block";
     }
 }
 );
+
+
+
+
+
+
+// create dots dynamically and add event listener
+class Dot {
+    constructor(index) {
+        this.index = index;
+        this.width = 20;
+        this.height = 20;
+        this.element = document.createElement("div");
+        this.element.classList.add("dot");
+        this.element.style.width = this.width + "px";
+        this.element.style.height = this.height + "px";
+        this.element.style.backgroundColor = 'white'
+        this.element.style.zIndex = '100'
+        this.element.style.margin = " auto 5px"
+        this.element.style.position = 'relative'
+        this.element.style.flex = "1"
+        this.element.style.borderRadius = "50%"
+        this.element.addEventListener("click", () => {
+            this.click();
+        });
+    }
+    click() {
+        curSlide = this.index;
+        slides.forEach((slide, index) => {
+            slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+            updateSlide(curSlide)
+            updateSlideEffect(curSlide);
+            nextSlide.style.display = "block";
+            prevSlide.style.display = "block";
+        });
+    }
+}
+
+
+// function to create dots
+var allDots = [];
+function createDots() {
+    var dotContainer = document.getElementById("dots");
+    for (let i = 0; i < totalNumbeOfFrame; i++) {
+        const dot = new Dot(i);
+        dotContainer.appendChild(dot.element);
+        allDots.push(dot.element);
+    }
+}
+
+
+// call create dots function
+createDots();
+
+// function to create the active effects on dot
+function updateSlideEffect(index){
+    // iterate through the dot
+    const myDots = document.getElementsByClassName('dot');
+    for(let i = 0; i < myDots.length; i++){
+        if(i == index){
+            // append the active class
+            myDots[i].classList.add('dotactive')
+        }else{
+            // remove the active class
+            myDots[i].classList.remove('dotactive')
+        }
+    }
+       
+        
+}
