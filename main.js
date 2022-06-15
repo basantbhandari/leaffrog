952163618a88c6badbb04f3ba02707170a75d2c6
basantbhandari <basantbhandari2074@gmail.com>
2022-06-15 23:39:30 +0545

    index table update

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+console.log(getRandomColor());
 var body = document.getElementsByTagName('body')[0];
-body.style.backgroundColor = `${getRandomColor()} !important`;
+body.style.backgroundColor = getRandomColor();
 
+console.log(body.style.backgroundColor);
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -9,3 +11,46 @@ function getRandomColor() {
     }
     return color;
 }
+// get table from html
+
+var myTable = document.getElementsByTagName('table')[0];
+myTable.style.backgroundColor = getRandomColor();
+myTable.style.width = '100%';
+
+// get h1 from html
+var myH1 = document.getElementsByTagName('h1')[0];
+myH1.style.backgroundColor = getRandomColor();
+myH1.style.textAlign = 'center';
+
+// get all hr by class name
+var myHr = document.getElementsByClassName('myHR');
+for (var i = 0; i < myHr.length; i++) {
+    myHr[i].style.color = getRandomColor();
+    myH1.style.border = '4px solid ' + getRandomColor();
+    myHr[i].style.backgroundColor = getRandomColor();
+}
+
+// get all  the th by tag name
+var myTh = document.getElementsByTagName('th');
+for (var i = 0; i < myTh.length; i++) {
+    myTh[i].style.backgroundColor = getRandomColor();
+    myTh[i].style.color = getRandomColor();
+    myTh[i].style.borderColor = getRandomColor();
+}
+
+
+// get all td by tag name
+
+
+var myTd = document.getElementsByTagName('td');
+for (var i = 0; i < myTd.length; i++) {
+    myTd[i].style.backgroundColor = getRandomColor();
+}
+
+
+// get footer by tag name
+var myFooter = document.getElementsByTagName('footer')[0];
+myFooter.style.backgroundColor = getRandomColor();
+myFooter.style.color = getRandomColor();
+
+
